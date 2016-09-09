205942e145b658678af020b4ae4d7fd592bfbce2
Ramiro Gómez <code@ramiro.org>
2016-09-10 00:58:01 +0200

    Document usage of nodeexport2logya.py. Leverage Logya API. Strip tags with regex so lxml is not needed. Code style.

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-drupal-converters
-=================
+# Drupal Converter Scripts
 
-A collection of drupal converter scrips mostly written in Python
+A collection of Drupal converter scrips written in Python.
+
+## nodeexport2logya.py
+
+This script processes a CSV dump created with the [node_export module](https://www.drupal.org/project/node_export).
+
+### Prerequisites
+
+Download and enable node_export using drush in the root directory of the Drupal site.
+
+    drush dl uuid
+    drush en uuid
+    drush dl node_export
+    drush en node_export
+    drush en node_export_dsv
+
+In the settings for node_export `admin/settings/node_export` enable `DSV` under `Format to use when exporting a node`. The export all nodes to a CSV file:
+
+    drush --format=dsv ne-export > nodes.csv
+
+For more information on how to use node_export with Drush run:
+
+    drush help ne-export
+
+Finally run the converter script in the root of a freshly created Logya site:
+
+    /path/to/nodeexport2logya.py /path/to/nodes.csv /path/to/drupal.map.py
--- nodeexport2logya.py
+++ nodeexport2logya.py
@@ -1,37 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# TODOs
-# convert HTML entities? see http://stackoverflow.com/a/12614706/291931
-# fix internal links
-# make sure all fields are included
+import argparse
+import csv
+import imp
 import os
 import re
-import imp
-import csv
-import yaml
-import argparse
-import StringIO
+
 from datetime import datetime
-from lxml import etree
 
-htmlparser = etree.HTMLParser()
+from logya import allowed_exts
+from logya.writer import write_content
 
-parser = argparse.ArgumentParser(description='node export converter')
-parser.add_argument('-f', required=True, help='Drupal Node export CSV file name')
-parser.add_argument('-m', required=True, help='File with mapping of Drupal fields to logya fields')
-args = parser.parse_args()
-
-def mkdir(name):
-    if not os.path.exists(name): os.makedirs(name)
 
 # http://stackoverflow.com/a/250373/291931
 def smart_truncate(content, length=100, suffix='...'):
     if len(content) <= length:
         return content
     else:
-        return ' '.join(content[:length+1].split(' ')[0:-1]) + suffix
+        return ' '.join(content[:length + 1].split(' ')[0:-1]) + suffix
 
-allowed_exts = ['html', 'htm', 'xml', 'json', 'js', 'css', 'php', 'md', 'markdown']
+
 def check_ext(path):
     for e in allowed_exts:
         if path.endswith(e):
@@ -39,22 +27,29 @@ def check_ext(path):
     return False
 
 
-re_ws = re.compile(r'\s+')
-fieldmap = {}
 docs = []
-mapping = imp.load_source('module.name', args.m)
+fieldmap = {}
+re_ws = re.compile(r'\s+')
+re_tag = re.compile(r'<[^<]+?>')
+
+parser = argparse.ArgumentParser(description='node export converter')
+parser.add_argument('csv_file', help='Drupal Node export CSV file name')
+parser.add_argument('mapping_file', help='File with mapping of Drupal fields to logya fields')
+args = parser.parse_args()
+
+mapping = imp.load_source('module.name', args.mapping_file)
 
 dirdst = 'content'
-mkdir(dirdst)
+os.makedirs(dirdst, exist_ok=True)
 
 dirtpl = 'templates'
-mkdir(dirtpl)
+os.makedirs(dirtpl, exist_ok=True)
 
-fcsv = open(args.f, 'rU')
+fcsv = open(args.csv_file, 'rU')
 reader = csv.reader(fcsv, quotechar='"', quoting=csv.QUOTE_ALL)
-headers = reader.next()
+headers = next(reader)
 
-# determine fields to be converted
+# Determine fields to be converted.
 for dfield, lfield in mapping.mapping.items():
     if dfield in headers:
         fieldmap[headers.index(dfield)] = lfield
@@ -64,18 +59,22 @@ for row in reader:
     doc = {}
     for idx, name in fieldmap.items():
         if idx < l and row[idx]:
-            doc[name] = row[idx].decode('utf-8')
+            doc[name] = row[idx]
 
     tags = []
     for t in mapping.tags:
         if t in doc:
-            if doc[t]: tags += doc[t].lower().split(', ')
+            if doc[t]:
+                tags += doc[t].lower().split(', ')
             del doc[t]
     doc['tags'] = tags
 
-    if 'path' not in doc or 'nid' not in doc: continue
+    if 'path' not in doc or 'nid' not in doc:
+        continue
+
     doc['url'] = '/' + doc['path'].lstrip('/')
-    # append slash if path does not end in allowed file extension
+
+    # Append slash if path does not end in allowed file extension.
     if not check_ext(doc['url']):
         doc['url'] = doc['url'].rstrip('/') + '/'
 
@@ -89,32 +88,27 @@ for row in reader:
 
     fnametpl = os.path.join(dirtpl, doc['template'])
     if not os.path.exists(fnametpl):
-        ftpl = open(fnametpl, 'w')
-        ftpl.write('')
-        ftpl.close()
+        with open(fnametpl, 'w') as f:
+            f.write('')
 
     if mapping.created in doc and doc[mapping.created]:
         doc[mapping.created] = datetime.fromtimestamp(int(doc[mapping.created]))
 
     body = doc['body'].strip()
     del doc['body']
-    for br in mapping.bodyremoves: body = body.replace(br, '')
+    del doc['nid']
 
-    desc = doc['description'].strip()
+    # Remove tokens in body.
+    for br in mapping.bodyremoves:
+        body = body.replace(br, '')
 
-    # strip HTML
-    tree = etree.parse(StringIO.StringIO(desc), htmlparser)
-    doc['description'] = etree.tostring(tree.getroot(), encoding=unicode, method='text')
+    # Strip HTML with regex, YES!
+    doc['description'] = re.sub(re_tag, ' ', doc.get('description', ''))
 
-    # replace multiple white space chars with one space
-    doc['description'] = re.sub(re_ws, ' ', doc['description'])
+    # Replace multiple white space chars with one space.
+    doc['description'] = re.sub(re_ws, ' ', doc['description']).strip()
 
-    # truncate to 160 chars, about the number Google shows in search results.
+    # Truncate to 160 chars, about the number Google shows in search results.
     doc['description'] = smart_truncate(doc['description'], length=160, suffix='')
 
-    fname = os.path.join(dirdst, doc['nid'] + '.html')
-    del doc['nid']
-    fdoc = open(fname, 'w')
-    fdoc.write('---\n%s---\n%s' % (yaml.safe_dump(doc), body.encode('utf-8')))
-    fdoc.close()
-
+    write_content(dirdst, doc, body)
